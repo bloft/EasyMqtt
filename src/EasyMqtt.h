b40df5b91470df3205370cd0bccb9616b6f50d7d
Bjarne Loft <bjarneloft@gmail.com>
2017-06-20 08:23:35 +0200

    Reformat code

--- src/EasyMqtt.h
+++ src/EasyMqtt.h
@@ -1,59 +1,63 @@
 #ifndef EasyMqtt_h
-#define EasyMqtt_h 
+#define EasyMqtt_h
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "MqttMap.h"
 
 class EasyMqtt : public MqttMap {
-	private:
-		WiFiClient wifiClient;
-		PubSubClient mqttClient;
-	
-		String deviceId = "EasyMqtt";	
-		const char* mqtt_username = "N/A";
-		const char* mqtt_password = "N/A";
+  private:
+    WiFiClient wifiClient;
+    PubSubClient mqttClient;
 
-	protected:
-		virtual String getTopic() {
-			return String("easyMqtt/") + deviceId;
-		}
+    String deviceId = "EasyMqtt";
+    const char* mqtt_username = "N/A";
+    const char* mqtt_password = "N/A";
 
-		MqttMap & getDiag() {
-			return operator[]("diag");
-		}
+  protected:
+    virtual String getTopic() {
+      return String("easyMqtt/") + deviceId;
+    }
 
-		/**
-		 * Handle connections to mqtt
-		 */
-		void mqttReconnect() {
-			while (!mqttClient.connected()) {
-				if (mqttClient.connect(deviceId.c_str(), mqtt_username, mqtt_password)) {
-					Serial.println("MQTT connected");
-					subscribe(mqttClient);
-					getDiag()["log"].publish(mqttClient, "connected to MQTT");
-				} else {
-					Serial.print("failed, rc=");
-					Serial.print(mqttClient.state());
-					Serial.println(" try again in 5 seconds");
-					delay(5000);
-				}
-			}
-		}
+    MqttMap & getDiag() {
+      return operator[]("diag");
+    }
 
-	public:
-		EasyMqtt() : MqttMap("easyMqtt") {
-			deviceId = String(ESP.getChipId());
+    /**
+       Handle connections to mqtt
+    */
+    void mqttReconnect() {
+      while (!mqttClient.connected()) {
+        if (mqttClient.connect(deviceId.c_str(), mqtt_username, mqtt_password)) {
+          Serial.println("MQTT connected");
+          subscribe(mqttClient);
+          getDiag()["log"].publish(mqttClient, "connected to MQTT");
+        } else {
+          Serial.print("failed, rc=");
+          Serial.print(mqttClient.state());
+          Serial.println(" try again in 5 seconds");
+          delay(5000);
+        }
+      }
+    }
+
+  public:
+    EasyMqtt() : MqttMap("easyMqtt") {
+      deviceId = String(ESP.getChipId());
 
       getDiag().setInterval(30);
-      getDiag()["mem"]["heap"] << [](){ return String(ESP.getFreeHeap()); };
-      getDiag()["uptime"] << [](){ return String(millis()); };
-		}
+      getDiag()["mem"]["heap"] << []() {
+        return String(ESP.getFreeHeap());
+      };
+      getDiag()["uptime"] << []() {
+        return String(millis());
+      };
+    }
 
-		/**
-		 * Setup connection to wifi
-		 */
-		void wifi(const char* ssid, const char* password) {
+    /**
+       Setup connection to wifi
+    */
+    void wifi(const char* ssid, const char* password) {
       WiFi.mode(WIFI_STA);
       WiFi.begin(ssid, password);
 
@@ -68,30 +72,36 @@ class EasyMqtt : public MqttMap {
       Serial.print("Chip ID : ");
       Serial.println(ESP.getChipId());
 
-			// Setup wifi diag
-			getDiag()["wifi"]["rssi"] << [](){ return String(WiFi.RSSI()); };
-      getDiag()["wifi"]["ssid"] << [](){ return WiFi.SSID(); };
-		}
+      // Setup wifi diag
+      getDiag()["wifi"]["rssi"] << []() {
+        return String(WiFi.RSSI());
+      };
+      getDiag()["wifi"]["ssid"] << []() {
+        return WiFi.SSID();
+      };
+    }
 
-		/**
-		 * Setup connection to mqtt
-		 */
-		void mqtt(const char* host, int port, const char* username, const char* password) {
-			mqttClient.setClient(wifiClient);
-			mqttClient.setCallback([&](const char* topic, uint8_t* payload, unsigned int length) { callback(topic, payload, length); });
-			mqttClient.setServer(host, port);
-			mqtt_username = username;
-			mqtt_password = password;
-		}
+    /**
+       Setup connection to mqtt
+    */
+    void mqtt(const char* host, int port, const char* username, const char* password) {
+      mqttClient.setClient(wifiClient);
+      mqttClient.setCallback([&](const char* topic, uint8_t* payload, unsigned int length) {
+        callback(topic, payload, length);
+      });
+      mqttClient.setServer(host, port);
+      mqtt_username = username;
+      mqtt_password = password;
+    }
 
-		/**
-		 * Handle the normal loop
-		 */
-		void loop() {
-			mqttReconnect();
-			mqttClient.loop();
-			MqttMap::loop(mqttClient);
-		}
+    /**
+       Handle the normal loop
+    */
+    void loop() {
+      mqttReconnect();
+      mqttClient.loop();
+      MqttMap::loop(mqttClient);
+    }
 };
 
 #endif
--- src/MqttMap.h
+++ src/MqttMap.h
@@ -1,15 +1,15 @@
 #ifndef MqttMap_h
-#define MqttMap_h 
+#define MqttMap_h
 
 #include <Arduino.h>
 #include <PubSubClient.h>
 
 class MqttMap {
-	private:
-		void (*outFunction)(String payload) = NULL;
-		String (*inFunction)() = NULL;
+  private:
+    void (*outFunction)(String payload) = NULL;
+    String (*inFunction)() = NULL;
 
-		const char* name = "N/A";
+    const char* name = "N/A";
     int interval = -1;
     unsigned long lastUpdate = 0; // Last read of data
 
@@ -17,7 +17,7 @@ class MqttMap {
     MqttMap* next = NULL;
     MqttMap* children = NULL;
 
-	protected:
+  protected:
     MqttMap(const char* _name, MqttMap& _parent) {
       name = _name;
       parent = &_parent;
@@ -25,119 +25,119 @@ class MqttMap {
     }
 
     virtual String getTopic() {
-      if(parent) {
+      if (parent) {
         return parent->getTopic() + "/" + name;
       } else {
         return String(name);
       }
     }
 
-	public:
-		MqttMap(const char* _name) {
-			name = _name;
+  public:
+    MqttMap(const char* _name) {
+      name = _name;
       interval = 5;
-		}
+    }
 
-		int getInterval() {
-      if(interval < 0) {
+    int getInterval() {
+      if (interval < 0) {
         return parent->getInterval();
       }
       return interval;
     }
 
-		void setInterval(int _interval) {
-			interval = _interval;
-		}
+    void setInterval(int _interval) {
+      interval = _interval;
+    }
 
-   void publish(PubSubClient& client, String message) {
+    void publish(PubSubClient& client, String message) {
       String topic = getTopic();
-      printf("publish[%s]: %s\n", topic.c_str(), message.c_str());
+      //      printf("publish[%s]: %s\n", topic.c_str(), message.c_str());
       client.publish(topic.c_str(), message.c_str());
     }
 
-		void callback(const char* _topic, uint8_t* payload, unsigned int length) {
-      if(strcmp(getTopic().c_str(), _topic) == 0) {
-        if(outFunction != NULL) {
-          char _payload[length];
-          for (int i=0;i<length;i++) {
-            _payload[i] = (char)payload[i];
+    void callback(const char* _topic, uint8_t* payload, unsigned int length) {
+      if (strcmp(getTopic().c_str(), _topic) == 0) {
+        if (outFunction != NULL) {
+          String _payload = "";
+          for (int i = 0; i < length; i++) {
+            _payload += (char)payload[i];
           }
-          printf("Found callback, %s: %s\n", _topic, _payload);
-          outFunction(String(_payload));
+          //        printf("Found callback, %s: %s\n", _topic, _payload.c_str());
+          outFunction(_payload);
         }
       } else {
         MqttMap* child = children;
-        while(child != NULL) {
+        while (child != NULL) {
           child->callback(_topic, payload, length);
           child = child->next;
         }
-      } 
-		}
+      }
+    }
 
-		/**
-		 *
-		 */
-		void subscribe(PubSubClient& client) {
-      if(outFunction != NULL) {
+    /**
+
+    */
+    void subscribe(PubSubClient& client) {
+      if (outFunction != NULL) {
         String topic = getTopic();
         printf("Subscribe: %s\n", topic.c_str());
         client.subscribe(topic.c_str());
       }
       MqttMap* child = children;
-      while(child != NULL) {
+      while (child != NULL) {
         child->subscribe(client);
         child = child->next;
       }
-		}
+    }
 
-		/**
-		 *
-		 */
-		void loop(PubSubClient& client) {
-      if(inFunction != NULL) {
+    /**
+
+    */
+    void loop(PubSubClient& client) {
+      if (inFunction != NULL) {
         unsigned long time = millis();
-        if(time >= (lastUpdate + (getInterval() * 1000))) {
-					lastUpdate = time;
-					String value = inFunction();
-					if(value != "") {
-						publish(client, value);
-					}
+        if (time >= (lastUpdate + (getInterval() * 1000))) {
+          lastUpdate = time;
+          String value = inFunction();
+          if (value != "") {
+            publish(client, value);
+          }
         }
       }
       MqttMap* child = children;
-        while(child != NULL) {
-          child->loop(client);;
-          child = child->next;
-        }
-		}
-
-		/**
-		 * 
-		 */
-		MqttMap & operator[](const char* name) {
-			MqttMap * child = children;
-        while(child != NULL) {
-          if(strcmp(child->name, name) == 0) {
-            return *child;
-          }
-          child = child->next;
-        }
-        MqttMap * oldChild = children;
-        children = new MqttMap(name, *this);
-        children->next = oldChild;
-        return *children;
-		}
+      while (child != NULL) {
+        child->loop(client);;
+        child = child->next;
+      }
+    }
 
     /**
-     * Read data from function and send it to mqtt
-     */
+
+    */
+    MqttMap & operator[](const char* name) {
+      MqttMap * child = children;
+      while (child != NULL) {
+        if (strcmp(child->name, name) == 0) {
+          return *child;
+        }
+        child = child->next;
+      }
+      MqttMap * oldChild = children;
+      children = new MqttMap(name, *this);
+      children->next = oldChild;
+      return *children;
+    }
+
+    /**
+       Read data from function and send it to mqtt
+    */
     void operator<<(String (*inFunction)()) {
       MqttMap::inFunction = inFunction;
     }
 
     /**
-     * Handle data comming from mqtt
-     */
+       Handle data comming from mqtt
+    */
     void operator>>(void (*outFunction)(String payload)) {
       MqttMap::outFunction = outFunction;
     }
