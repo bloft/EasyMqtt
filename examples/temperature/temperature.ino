85c65f86ba9e960aa911aea46694e6d38a025ed7
Bjarne Loft <bjarneloft@gmail.com>
2017-08-16 15:33:34 +0200

    Add more exambles

---
+++ examples/temperature/temperature.ino
@@ -0,0 +1,34 @@
+#include <EasyMqtt.h>
+#include <DHT.h>
+
+EasyMqtt mqtt;
+DHT dht(D7, DHT22);
+
+void setup() {
+  dht.begin();
+  mqtt.wifi("ssid", "pass");
+  mqtt.mqtt("server", 1883, "user", "password");
+
+  mqtt["temperature"] << []() {
+    float value = dht.readTemperature();
+    if(isnan(value)) {
+      return String("");
+    } else {
+      return String(value);
+    }
+  }
+
+  mqtt["humidity"] << []() {
+    float value = dht.dht.readHumidity();
+    if(isnan(value)) {
+      return String("");
+    } else {
+      return String(value);
+    }
+  }
+
+}
+
+void loop() {
+  mqtt.loop();
+}
