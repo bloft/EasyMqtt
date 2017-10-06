fe39fe4ba8ee517ab810bd3d3284b9d0267745c8
Bjarne Loft <bjarneloft@gmail.com>
2017-10-06 14:03:21 +0200

    OTA support

--- src/EasyMqtt.h
+++ src/EasyMqtt.h
@@ -2,6 +2,9 @@
 #define EasyMqtt_h
 
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include "MqttEntry.h"
 #include "WebPortal.h"
@@ -132,12 +135,34 @@ class EasyMqtt : public MqttEntry {
       #endif
     }
 
+    void ota() {
+      ArduinoOTA.onStart([]() {
+        Serial.println("OTA - Start");
+      });
+      ArduinoOTA.onEnd([]() {
+        Serial.println("OTA - End");
+      });
+      ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("OTA - Progress: %u%%", (progress / (total / 100)));
+      });
+      ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      });
+      ArduinoOTA.begin();
+    }
+
     /**
        Handle the normal loop
     */
     void loop() {
       mqttReconnect();
       mqttClient.loop();
+      // ArduinoOTA.handle();
       webPortal.loop();
       each([](MqttEntry* entry){
           entry->update();
